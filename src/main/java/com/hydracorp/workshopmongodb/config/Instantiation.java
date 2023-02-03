63b284c9aa507f00961b2a2725282f1b95f67d81
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-02-03 11:25:18 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/hydracorp/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/hydracorp/workshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.hydracorp.workshopmongodb.domain.Post;
 import com.hydracorp.workshopmongodb.domain.User;
 import com.hydracorp.workshopmongodb.dto.AuthorDTO;
+import com.hydracorp.workshopmongodb.dto.CommentDTO;
 import com.hydracorp.workshopmongodb.repository.PostRepository;
 import com.hydracorp.workshopmongodb.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("02/02/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("06/06/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO( "Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("2018/03/22"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("2018/03/23"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
