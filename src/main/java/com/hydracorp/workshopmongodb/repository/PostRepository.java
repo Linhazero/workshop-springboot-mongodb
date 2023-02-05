c656af4c4f169565f11170a86609babb8132e406
Victor Rodrigues <victorxdrodrigues@gmail.com>
2023-02-05 10:16:17 -0300

    Consulta simples com @Query

--- src/main/java/com/hydracorp/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/hydracorp/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.hydracorp.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hydracorp.workshopmongodb.domain.Post;
@@ -10,5 +11,8 @@ import com.hydracorp.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searcTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
